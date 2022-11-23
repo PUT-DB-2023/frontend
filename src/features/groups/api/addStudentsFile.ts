f9ec8d23a52f51937bfa94d68bff4e048cea33d0
Kamileoo <kamil2000a@gmail.com>
2022-11-23 20:21:34 +0100

    Adding students from file

---
+++ src/features/groups/api/addStudentsFile.ts
@@ -0,0 +1,14 @@
+import { axios } from 'lib/axios'
+import { toast } from 'react-toastify'
+
+export const addStudentsFile = async (data: FormData) => {
+    const t = toast.loading("Dodawanie...")
+    const response = await axios({
+        method: 'post',
+        url: '/load_students_csv',
+        data: data
+    })
+    .then((e)=>{toast.update(t, {render: "Pomyślnie dodano studentów", type: "success", isLoading: false, closeButton: true, autoClose: 5000}); return e})
+    .catch((e)=>{toast.update(t, {render: "Nie udało się dodać studentów", type: "error", isLoading: false, closeButton: true, autoClose: 5000}); return e})
+    return response.data
+}
---
+++ src/features/groups/components/AddStudCSVModal.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { ModalContainer } from 'components/ModalContainer';
+import { Button } from 'components/Button';
+import { ButtonType } from 'types';
+import { FieldBox } from 'components/FieldBox';
+import { addStudentsFile } from '../api/addStudentsFile';
+
+export const AddStudCSVModal = ({ show, off, refetch, id }: { show: boolean, off: () => void, refetch: () => void, id: string }) => {
+    const [students, setStudents] = React.useState(undefined);
+
+    const handleOff = React.useCallback(() => {
+        setStudents(undefined);
+        off();
+    }, [])
+
+    const handleAdd = React.useCallback(async () => {
+        if (students !== undefined) {
+            const data = new FormData();
+            data.append('group_id', id);
+            data.append('students_csv', students)
+            const res = await addStudentsFile(data);
+            if (res) {
+                handleOff()
+                refetch()
+            }
+        }
+    }, [students, id])
+
+    const handleSelectedFile = React.useCallback((event: any) => {
+        setStudents(event.target.files[0]);
+    }, []);
+
+    if (show) {
+        return (
+            <ModalContainer title='Dodaj studentów z pliku CSV' off={handleOff}>
+                <div className={`flex flex-col gap-1`}>
+                    <FieldBox>
+                        <input className="block w-full text-zinc-600 border border-zinc-400 rounded-lg cursor-pointer focus:outline-none focus-visible:outline-blue-800"
+                            type="file" accept=".csv, .xlsx" onChange={handleSelectedFile} />
+                    </FieldBox>
+                </div>
+                <div className={`flex gap-2 mt-10 self-end`}>
+                    <Button type={ButtonType.OUTLINE} text='Anuluj' onClick={handleOff} />
+                    <Button type={ButtonType.ACTION} text='Dodaj' onClick={handleAdd} />
+                </div>
+            </ModalContainer>
+        );
+    } else {
+        return null;
+    }
+};
+
+AddStudCSVModal.displayName = 'AddStudCSVModal';
