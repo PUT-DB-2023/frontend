9d5418a517766052beed30f02240d3f4ea65231e
Przemyslaw Wozniak <wozprzemek@gmail.com>
2022-11-24 23:01:13 +0100

    Added semester activation toasts.

--- src/features/semesters/api/activateSemester.tsx
+++ src/features/semesters/api/activateSemester.tsx
@@ -1,13 +1,13 @@
 import { axios } from 'lib/axios'
+import { toast } from 'react-toastify';
 import { Semester } from '../types'
 
 export const activateSemester = async (semesterId: string|undefined) => {
-    console.log('ACTIVATE API');
-    
+    const t = toast.loading("Aktywowanie semestru..")
     const response = await axios.post(`/change_active_semester`, {
         'semester_id': semesterId  
     })
-    .then(e => {return e})
-    .catch(e => {return e})
+    .then((e)=>{toast.update(t, {render: "Pomyślnie aktywowano semestr", type: "success", isLoading: false, closeButton: true, autoClose: 5000}); return e})
+    .catch((e)=>{toast.update(t, {render: "Nie udało się aktywować semestru", type: "error", isLoading: false, closeButton: true, autoClose: 5000}); return e})
     return response.data
 }
