bcd87352d23e8d10e347d84f83c0637a6e3e8323
Przemyslaw Wozniak <wozprzemek@gmail.com>
2022-12-08 12:48:40 +0100

    Removed sidebar navigation accordion menus and data fetching

--- src/components/SideNavigation.tsx
+++ src/components/SideNavigation.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png'
 import { Disclosure } from '@headlessui/react'
@@ -14,135 +14,39 @@ import { Spinner } from './Spinner'
 import { getGroups } from 'features/groups/api/getGroups'
 import { getSemesters } from 'features/semesters/api/getSemesters'
 
-interface AccordionMenuProps {
+interface INavItem {
   title: string;
-  url: string;
-  icon: React.ReactNode;
-  children?: any[];
-  userMenu? : boolean;
-};
+  icon: ReactNode;
+}
 
-export const AccordionMenu = ({title, url, icon, children, userMenu} : AccordionMenuProps) => {
+const NavItem = ({title, icon} : INavItem) => {
   return (
-    <Disclosure>
-      {({ open } : {open : boolean}) => (
-        <>
-          <Disclosure.Button className="flex w-full items-center justify-between py-2 pl-6 pr-4 hover:bg-blue-600 transition-all">
-              <div className='flex items-center space-x-4'>
-                  {icon}
-                  <Link to={ url } className='hover:text-slate-200 hover:underline transition-all'>{title}</Link>
-              </div>
-            <ChevronUpIcon
-              className={`${
-                open ? 'rotate-180 transform' : 'rotate-90 transform'
-              } h-5 w-5 text-white`}
-            />
-          </Disclosure.Button>
-          {children?.length  && !userMenu ?
-              children.map((object, index) => {
-                  return index != children.length -1 ? 
-                  <Disclosure.Panel key={index} className="px-10 pt-2 ml-6 pb-2 text-sm text-white hover:bg-blue-600 hover:cursor-pointer transition-all"><Link to={url + object.id}>
-                    {object.year ? `${object.year} ${object.winter ? '- Zima' : '- Lato'}` : object.name}
-                  </Link></Disclosure.Panel> : 
-                  <Disclosure.Panel key={index} className="px-10 pt-2 ml-6 pb-2 mb-4 text-sm text-white hover:bg-blue-600 hover:cursor-pointer transition-all"><Link to={url + object.id}>
-                    {object.year ? `${object.year} ${object.winter ? '- Zima' : '- Lato'}` : object.name}
-                  </Link></Disclosure.Panel>
-              }) : null
-          }
-          {userMenu ?
-            <> 
-              <Link to='/users/admins/'><Disclosure.Panel className="px-10 pt-2 ml-6 pb-2 text-sm text-white hover:bg-blue-600 hover:cursor-pointer transition-all">
-                Administratorzy
-              </Disclosure.Panel></Link>
-              <Link to='/users/teachers/'><Disclosure.Panel className="px-10 pt-2 ml-6 pb-2 text-sm text-white hover:bg-blue-600 hover:cursor-pointer transition-all">
-                Dydaktycy
-              </Disclosure.Panel></Link>
-              <Link to='/users/students/'><Disclosure.Panel className="px-10 pt-2 ml-6 pb-2 text-sm text-white hover:bg-blue-600 hover:cursor-pointer transition-all">
-                Studenci
-              </Disclosure.Panel></Link>
-              </>
-              : null
-          }
-        </>
-      )}
-    </Disclosure>
+    <div className='flex w-full items-center gap-5 py-2 pl-6 pr-4 hover:bg-blue-600 transition-all'>
+      {icon}
+      {title}
+    </div>
   )
 }
 
 export const SideNavigation = () => {
-  const { data: courseData, status: courseStatus, refetch: courseRefetch } = useQuery(['courses'], () => getCourses(undefined))
-  const { data: serverData, status: serverStatus, refetch: serverRefetch } = useQuery('servers', () => getServers(undefined))
-  const { data: groupData, status: groupStatus, refetch: groupRefetch } = useQuery('groups', getGroups)
-  const { data: semesterData, status: semesterStatus, refetch: semesterRefetch } = useQuery('semesters', () => getSemesters(false))
-
-  let coursesContent = null
-  let serversContent = null
-  let groupsContent = null
-  let semestersContent = null
-
-  if (courseStatus === 'loading') {
-    coursesContent = (
-      <div className='w-full h-full flex items-center justify-center'>
-        <Spinner />
-      </div>
-    );
-  }
-
-  else if (courseStatus === 'success') {
-      coursesContent = (
-          <AccordionMenu title='Przedmioty' url='/courses/' icon={<AcademicCapIcon className='h-5 w-auto'/>} children={courseData}/>
-      )
-  }
-
-  if (serverStatus === 'loading') {
-    coursesContent = (
-      <div className='w-full h-full flex items-center justify-center'>
-        <Spinner />
-      </div>
-    )
-  }
-
-  else if (serverStatus === 'success') {
-    serversContent = (
-      <AccordionMenu title='Serwery' url='/servers/' icon={<DatabaseIcon className='h-5 w-auto'/>} children={serverData}/>
-    )
-  }
-
-  if (groupStatus === 'loading') {
-    groupsContent = (
-      <div className='w-full h-full flex items-center justify-center'>
-        <Spinner />
-      </div>
-    )
-  }
-
-  else if (groupStatus === 'success') {
-    groupsContent = (
-      <AccordionMenu title='Grupy' url='/groups/' icon={<UserGroupIcon className='h-5 w-auto'/>} children={groupData}/>
-    )
-  }
-
-  if (semesterStatus === 'loading') {
-    semestersContent = (
-      <div className='w-full h-full flex items-center justify-center'>
-        <Spinner />
-      </div>
-    )
-  }
-
-  else if (semesterStatus === 'success') {
-    semestersContent = (
-      <AccordionMenu title='Semestry' url='/semesters/' icon={<CalendarIcon className='h-5 w-auto'/>} children={semesterData}/>
-    )
-  }
 
   return (
     <nav className='flex flex-col w-full h-full overflow-y-auto mt-12 scrollbar-hide'>
-        { coursesContent }
-        { serversContent }
-        <AccordionMenu title='Użytkownicy' url='/users/' icon={<UserIcon className='h-5 w-auto'/>} userMenu={ true }/>
-        { groupsContent }
-        { semestersContent }
+        <Link to='/courses/'>
+          <NavItem title='Przedmioty' icon={<UserIcon className='h-5 w-auto'/>} />
+        </Link>
+        <Link to='/servers/'>
+          <NavItem title='Serwery' icon={<DatabaseIcon className='h-5 w-auto'/>} />
+        </Link>
+        <Link to='/users/'>
+          <NavItem title='Użytkownicy' icon={<UsersIcon className='h-5 w-auto'/>} />
+        </Link>
+        <Link to='/groups/'>
+          <NavItem title='Groups' icon={<UserGroupIcon className='h-5 w-auto'/>} />
+        </Link>
+        <Link to='/semesters/'>
+          <NavItem title='Semestry' icon={<CalendarIcon className='h-5 w-auto'/>} />
+        </Link>
     </nav>
   )
 }
