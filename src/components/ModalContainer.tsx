d6cc05f5e34e18c68e7beb942ebfe0798ce6e5d3
Kamileoo <kamil2000a@gmail.com>
2022-12-10 21:15:47 +0100

    Better modal

--- src/components/ModalContainer.tsx
+++ src/components/ModalContainer.tsx
@@ -16,10 +16,10 @@ export const ModalContainer: React.FC<IModalContainer> = ({
     return (
         <div className={`absolute w-full h-full top-0 left-0 overflow-x-hidden overflow-y-auto z-20 bg-black/25`} >
             <div className={`flex w-full h-full p-[20px]`}>
-                <div className={`max-h-full lg:min-w-[460px] md:lg:min-w-[460px] overflow-auto flex flex-col gap-4 p-6 bg-white shadow-md rounded-md m-auto justify-between`} ref={ref} style={style}>
-                        <div className={`font-semibold text-lg`} >{title}</div>
-                        <div className='overflow-y-auto'>{children}</div>
-                        {buttons ?? <div>{buttons}</div>}
+                <div className={`max-h-full lg:min-w-[460px] md:lg:min-w-[460px] overflow-auto flex flex-col gap-4 bg-white shadow-md rounded-md m-auto justify-between`} ref={ref} style={style}>
+                        <div className={`font-semibold text-lg px-6 pt-6`} >{title}</div>
+                        <div className={`overflow-y-auto px-6 ${buttons ? '' : 'pb-6'}`}>{children}</div>
+                        {buttons ?? <div className='pb-6'>{buttons}</div>}
                 </div>
             </div>
         </div>
