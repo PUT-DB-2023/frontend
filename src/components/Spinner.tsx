2b9a60433d4081e4036e0c20798b9e1bf8d2b3b3
Kamileoo <kamil2000a@gmail.com>
2022-12-30 01:21:56 +0100

    Repaired spinner

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -2,7 +2,7 @@ export const Spinner = () => {
   return (
     <>
       <svg
-        className='h-8 w-8 text-blue-600 self-center justify-self-center'
+        className='h-8 w-8 text-blue-600 self-center justify-self-center animate-spin'
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
