78227214187e4ef786a69820585554aabc93c326
Kamileoo <kamil2000a@gmail.com>
2023-01-08 15:51:08 +0100

    Added logout toasts

--- src/features/auth/api/logout.ts
+++ src/features/auth/api/logout.ts
@@ -1,10 +1,13 @@
 import { axios } from "lib/axios";
+import { toast } from 'react-toastify'
+import { displayError } from "api/displayError";
 
 export const logout = async () => {
 
+  const t = toast.loading("Wylogowywanie...")   
   const response = await axios.post('/logout')
-  .then((e)=>{return e})
-  .catch((e)=>{return e})    
+  .then((e)=>{toast.update(t, {render: `Pomyślnie wylogowano`, type: "success", theme: "colored", isLoading: false, closeButton: true, autoClose: 4000, position: 'bottom-right'}); return e})
+  .catch((e)=>{toast.update(t, {render: `Nie udało się wylogować - ${displayError(e.response.data)} - ${displayError(e.response.data)}`, type: "error", theme: "colored", isLoading: false, closeButton: true, autoClose: 8000, position: 'bottom-right'}); return e})  
 
   return response.data
 };
