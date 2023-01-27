d638c918eb55322187e50d4da2bc995a07048e65
Kamileoo <kamil2000a@gmail.com>
2023-01-27 02:43:44 +0100

    Fixed incorrect field in Course Edit Modal (Description)

--- src/features/courses/components/EditModal.tsx
+++ src/features/courses/components/EditModal.tsx
@@ -76,7 +76,7 @@ export const EditModal = ({ show, off, refetch, data }: IEditModal) => {
                 <div className={`flex flex-col gap-1`}>
                     <Field title={"Nazwa"} value={name} setValue={setName} autoFocus={true} errorMsg={errorMsg['name']} setErrorMsg={(e: string) => setErrorMsg(prevState => ({ ...prevState, 'name': e }))} maxLenght={50}/>
                     {majorsData && <MajorsDropDown title='Kierunek' values={majorsData} value={major} setValue={setMajor} errorMsg={errorMsg['major']} setErrorMsg={(e: string) => setErrorMsg(prevState => ({ ...prevState, 'major': e }))}/>}
-                    <Field title={"Opis"} value={description} setValue={setDescription} maxLenght={255}/>
+                    <Field title={"Opis"} value={description} setValue={setDescription} multiline={true} maxLenght={255}/>
                 </div>
             </ModalContainer>
         );
